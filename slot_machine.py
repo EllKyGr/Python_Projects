a9c46df5bf2a2bdb4a2ac0cac935f3a0a380efa9
EllKyGr <156482345+EllKyGr@users.noreply.github.com>
2024-11-22 16:05:00 -0600

    Add symbols dictionary for organization and visual appealing Refactor existing functions Modify documentation accordingly

--- slot_machine.py
+++ slot_machine.py
@@ -1,20 +1,73 @@
+# Create a text based slot machine
+from random import choice
 from typing import Any
-from random import randint
+from time import sleep
 
-# Program that allows the user to bet money by:
-# - Making a deposit
-# - Make a bet, i.e., match one, two or three of the slots
-# - Spin wheel
-# - Asses the results
-# - If there's a match the multiply the current value by x
-# - if no match the bet is loss and whatever amount was, reduced it from total
-# - User then can choose to play again or withdraw
-# - If user looses all the money the game ends
+MAX_LINES: int = 3
+MIN_LINES: int = 1
+MAX_BET: int = 100
+MIN_BET: int = 5
+REELS: int = 3
+ROWS: int = 3
+
+# 10 symbols in total per reel
+SYMBOLS = {
+    "cherry": {
+        "unicode": "\U0001F352",
+        "count": 1,
+        "value": 2,
+    },
+    "tangerine": {
+        "unicode": "\U0001F34A",
+        "count": 1,
+        "value": 2,
+    },
+    "lemon": {
+        "unicode": "\U0001F34B",
+        "count": 1,
+        "value": 2,
+    },
+    "peach": {
+        "unicode": "\U0001F351",
+        "count": 1,
+        "value": 2,
+    },
+    "grapes": {
+        "unicode": "\U0001F347",
+        "count": 1,
+        "value": 2,
+    },
+    "watermelon": {
+        "unicode": "\U0001F349",
+        "count": 1,
+        "value": 2,
+    },
+    "bell": {
+        "unicode": "\U0001F514",
+        "count": 1,
+        "value": 3,
+    },
+    "seven": {
+        "unicode": "7\uFE0F\u20E3 ",
+        "count": 1,
+        "value": 4,
+    },
+    "cash": {
+        "unicode": "\U0001F4B0",
+        "count": 1,
+        "value": 5,
+    },
+    "diamond": {
+        "unicode": "\U0001F48E",
+        "count": 1,
+        "value": 6,
+    },
+}
 
 
 def initial_deposit() -> int:
     """
-    The starting funds for the game.
+    The starting funds in order to play.
 
     :return: the monetary value needed to play the game.
     :rtype: int
@@ -25,7 +78,7 @@ def initial_deposit() -> int:
             print("Invalid entry.")
             continue
         deposit = int(deposit)
-        if deposit > 100:
+        if deposit >= 100:
             break
         else:
             print("Initial deposit must be at least $100.00")
@@ -35,104 +88,147 @@ def initial_deposit() -> int:
     return deposit
 
 
-def set_bet(deposit: int) -> list[int]:
+def set_bet(funds: int, lines: int) -> list[int]:
     """
     Set the amount of money the user is willing to bet.
 
-    :return: the bet and remaining quantity from the original funds.
+    :param funds: current credit of user
+    :type lines: int
+    :param funds: number of lines bet on by user
+    :type lines: int
+    :return: the bet and remaining funds.
     :rtype: list[int]
     """
-    while True:
-        bet: Any = input("How much would you want to bet? ")
+    while True:  # Save the first bet value if you don't want to change it
+        bet: Any = input("How much would you want to bet on each line? ")
         if not bet.isdigit():
             print("Invalid entry.")
             continue
         bet = int(bet)
-        deposit -= bet
-        break
+        # Rigged amount of bet per line
+        if MIN_BET <= bet <= MAX_BET:
+            total_bet: int = lines * bet
+            if total_bet > funds:
+                print(f"Insufficient funds for bet.\nCurrent funds: ${funds}")
+                continue
+            break
+        else:
+            print(f"Bet must be between ${MIN_BET} - ${MAX_BET}")
 
-    print(f"You're bet is ${bet}")
+    funds -= total_bet
 
-    return [bet, deposit]
+    print(f"You're bet is ${bet} in {lines} lines.\nTotal bet ${total_bet}\n")
+
+    return [total_bet, funds]
 
 
-def user_choices() -> list[str]:
+def predefined_symbols(symbols: dict, reels: int, rows: int) -> list[list]:
     """
-    Users input for the bet comparison. Might me removed at the end.
+    Random generated matrix composed of three reels containing the slot symbols
 
-    :return: slot values selected by the user 1, 2 or 3.
-    :rtype: list[str]
+    :param symbols: associated symbol with its unicode representation, count
+                    and value
+    :type symbols: dict[str, dict]
+    :param reels: constant value of total reels
+    :type reels: int
+    :param rows: constant value of total rows
+    :type rows: int
+    :return: matrix containing the reels
+    :rtype: list[list]
+    :example:
+    >>> [['🍉', '🍋', '🍊'], ['🔔', '🍊', '🍒'], ['🍒', '🍑', '🔔']]
     """
-    # Which symbols (3?) per slot (3 too), i.e., 1-1-1 or 3-1-3
-    user_bet: list[str] = []
-    count: int = 0
-    while count < 3:
-        slot: str = input(f"Slot number {count + 1}: ")
-        if slot not in ["1", "2", "3"]:
-            print("Invalid")
-            continue
-        user_bet.append(slot)
-        count += 1
+    total_reels: list[list] = []
+    list_of_symbols: list[str] = []
+    for sym in symbols.values():
+        list_of_symbols.append(sym["unicode"])
+    for _ in range(reels):
+        inner_reel: list = []
+        current_list_of_symbols: list[str] = list_of_symbols[:]
+        for _ in range(rows):
+            symbol: str = choice(current_list_of_symbols)
+            current_list_of_symbols.remove(symbol)
+            inner_reel.append(symbol)
 
-    return user_bet
+        total_reels.append(inner_reel)
+
+    return total_reels
 
 
-def pull_lever() -> list[str]:
+def output_spin(spin: list[list]) -> None:
     """
-    Generates a random combination in comparison to the user selection. This
-    might be take solely place with previous function.
+    Visual representation of the spin after users action
+
+    :param spin: matrix representing the three reels from the slot machine
+    :type spin: list[list]
+    :return: None
+    :example:
+    >>> \n
+        🍉 | 🔔 | 🍒\n
+        🍋 | 🍊 | 🍑\n
+        🍊 | 🍒 | 🔔
+    """
+    for reel in range(len(spin)):
+        for idx, value in enumerate(spin):
+            if idx != len(spin) - 1:
+                print(value[reel], end=" | ")
+            else:
+                print(value[reel], end="")
+            sleep(0.07)
+
+        print()
+
+
+def pull_lever() -> list[list]:
+    """
+    A "spin" takes place generating three different reels with random symbols
 
     :return: the random values for every slot
     :rtype: list[str]
     """
-    options: list[str] = ["1", "2", "3"]
-    pc_bet: list[str] = []
     while True:
-        pull: str = input("Pull the lever! (Enter) ")
+        pull: str = input("Pull the lever! (Enter) \n")
         if pull != "":
             continue
-        # Make the machine output a value in order to compare with user choice
-        for _ in range(3):
-            outcome: int = randint(int(min(options)), int(max(options)))
-            pc_bet.append(str(outcome))
+        reel_outcome: list[list] = predefined_symbols(SYMBOLS, REELS, ROWS)
+        output_spin(reel_outcome)
+        print()
         break
 
-    print(f"{pc_bet[0]} | {pc_bet[1]} | {pc_bet[2]}")
-
-    return pc_bet
+    return reel_outcome
 
 
-def jackpot(bet: int, deposit: int) -> int:
+def jackpot(all_reels: list[list], bet: int, lines: int) -> list:
     """
-    Compares user input with the random generated options in order to determine
-    specific earnings based on matching slots. If all slots match the earnings
-    are doubled; first and third matches gives one and half, anything else
-    yields no earnings.
+    Assess the combinations from the spin in order to return, if any, earnings.
 
-    :param bet: users bet
+    :param all_reels: a group of three random generated reels inside a matrix
+    :type all_reels: list[list]
+    :param bet: the users bet
     :type bet: int
-    :param deposit: current funds from user
-    :type deposit: int
-    :return: new value from deposit
+    :param lines: lines selected by the user upon which the bet takes place
+    :type lines: int
+    :return: the total earnings after the spin
     :rtype: int
     """
-    # Which combinations are the winner ones?
-    # All the same -> 2x times earnings => bet of 100 returns 200
-    # First and third -> 1.5x times earnings => bet of 100 returns 150
-    # Anything else -> 0 earnings => bet of 100 returns nothing
-    user_bet, pc_bet = user_choices(), pull_lever()
-    if user_bet == pc_bet:
-        print("Winner!")
-        bet *= 2
-    elif user_bet[0] == pc_bet[0] and user_bet[2] == pc_bet[2]:
-        print("Winner!")
-        bet = round(bet * 1.5)
-    else:
-        print("Looser!")
-        bet = 0
+    winning_lines: list = []
+    earnings: int = 0
+    # Since bet is based on lines, it will only iterate accordingly
+    for line in range(lines):
+        # Based on the first reel compare to the following reel, same position,
+        # (idx 0, 1, 2) if it is the same symbol. Break if not
+        symbol: str = all_reels[0][line]
+        for reel in all_reels:
+            symbol_to_check: str = reel[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            earnings += bet * multiplier_bonus(symbol)
+            winning_lines.append(line)
 
-    deposit += bet
-    return deposit
+    print("Jackpot!" if earnings > 0 else "Not a chance!")
+
+    return [earnings, winning_lines]
 
 
 def bet_or_cash(funds: int) -> int:
@@ -145,8 +241,6 @@ def bet_or_cash(funds: int) -> int:
     :return: modified funds value
     :rtype: int
     """
-    # It will only ask once for the initial funding, said value is later used
-    # for the rest of the gambling experience
     while True:
         if funds == 0:
             print("Game over!")
@@ -155,10 +249,12 @@ def bet_or_cash(funds: int) -> int:
         if choice == "q":
             break
         elif choice == "p":
-            bet, deposit = set_bet(funds)
-            new_funds: int = jackpot(bet, deposit)
-            funds = new_funds
-            print(f"You're new funds are ${funds}")
+            lines: int = get_number_lines()
+            bet, current_funds = set_bet(funds, lines)
+            new_funds, w_lines = jackpot(pull_lever(), bet, lines)
+            funds = current_funds + new_funds
+            print(f"You're new funds are ${funds}.")
+            print("You won on lines", *w_lines)
         else:
             continue
     return funds
@@ -173,10 +269,11 @@ def start_game() -> None:
     :rtype: NoneType
     """
     print("Starting Jackpot")
+    # Ask once for the initial balance
     funds: int = initial_deposit()
     earnings: int = bet_or_cash(funds)
     if earnings > 0:
-        print(f"You're final earnings are ${earnings}")
+        print(f"You're final funds are ${earnings}")
     else:
         print("You ran out of funds, best of luck next time!")
 
